ec0b089450af744f500f386c3e65ac995b4636a1
koykhaled <koykhaled@gmail.com>
2023-10-22 15:40:27 +0300

    add put,patch for user groups

--- resturant/urls.py
+++ resturant/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from .views import GroupMemeberView
 
 urlpatterns = [
-    path("user-group/",GroupMemeberView.as_view()),
-    path("user/<int:user_id>/group",GroupMemeberView.as_view()),
+    path("user/group",GroupMemeberView.as_view()),
+    path("user/<int:user_id>/group/",GroupMemeberView.as_view()),
     path("user/<int:user_id>/group/<int:group_id>/",GroupMemeberView.as_view()),
 
 ]
--- resturant/views.py
+++ resturant/views.py
@@ -16,15 +16,17 @@ from django.contrib.auth.models import User , Group
 class GroupMemeberView(APIView):
     permission_classes = [IsAuthenticated,IsManager]
     serializer_class = GroupMemberSerializer
-    
+    # get users that assigne to groups
     def get(self,request):
         
         user_group = GroupMembership.objects.all()
         serializer = self.serializer_class(user_group,many=True)
         if len(serializer.data)  :
             return Response({"data":serializer.data},status=status.HTTP_200_OK)
-        return Response({"message" : "no user in Group"},status=status.HTTP_404_NOT_FOUND)
+        return Response({"message" : "no Users in Groups"},status=status.HTTP_404_NOT_FOUND)
     
+    
+    # assign users to groups
     def post(self,request,user_id):
         user = get_object_or_404(User,id=user_id)
         group_id = request.data.get('group')
@@ -36,6 +38,38 @@ class GroupMemeberView(APIView):
         user_group.save()
         return Response({"message" : "adding user to group done"},status=status.HTTP_200_OK)
     
+    
+    def put(self, request, user_id,group_id):
+        user = get_object_or_404(User, id=user_id)
+        group = get_object_or_404(Group, id=group_id)
+        user_group = get_object_or_404(GroupMembership,user=user,group=group)
+        new_group = get_object_or_404(Group, id=request.data.get('group'))
+
+        user_group.group = new_group
+        user_group.save()
+
+        serializer = GroupMemberSerializer(user_group)
+        return Response({"data":serializer.data,"message":"update User Group Done"},status=status.HTTP_200_OK)
+
+        
+    def patch(self, request, user_id,group_id):
+        try :
+            user = get_object_or_404(User, id=user_id)
+            group = get_object_or_404(Group, id=group_id)
+            user_group = get_object_or_404(GroupMembership,user=user,group=group)
+            new_group = get_object_or_404(Group, id=request.data.get('group'))
+
+            user_group.group = new_group
+            user_group.save()
+
+            serializer = GroupMemberSerializer(user_group)
+            return Response({"data":serializer.data,"message":"update User Group Done"},status=status.HTTP_200_OK)
+        except Exception as e:
+            return Response({"message":str(e)},status=status.HTTP_400_BAD_REQUEST)
+
+            
+    
+    # delete users from groups
     def delete(self,request,user_id,group_id):
         try:
             user = GroupMembership.objects.filter(user=user_id,group=group_id)
